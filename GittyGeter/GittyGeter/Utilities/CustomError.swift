46631edadd00266188a52e97c0ad561204632a11
Petar Perkovski <petar.perkovski@gmail.com>
2024-11-26 12:58:23 +0100

    Organization

---
+++ GittyGeter/GittyGeter/Utilities/CustomError.swift
@@ -0,0 +1,21 @@
+//
+//  CustomError.swift
+//  GittyGeter
+//
+//  Created by Petar Perkovski on 26/11/2024.
+//
+
+import Foundation
+
+enum CustomError: Error {
+    case dataMappingFailed,
+         unknown(Error)
+}
+
+extension CustomError {
+    static
+    func from(any error: Error) -> CustomError {
+        //handle it better when you have the time
+        .unknown(error)
+    }
+}
